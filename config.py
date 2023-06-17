14fa43fd3e5188f3b9f521d61eeb1c69de040721
xneoSavage <xneo.savage@gmail.com>
2023-06-17 16:47:18 +0300

    change in conf file, prep to deploy

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ basedir = os.path.abspath(__file__)
 class Config(object):
 	SECRET_KEY = 'SECRET_KEY'
 	# db connection
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:savage@localhost/mono_flask'
+	#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2332@localhost/monoFlask'
+	SQLALCHEMY_DATABASE_URI = os.getenv('DB_URI')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
